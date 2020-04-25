72989c21a593e733071a08687588d218dce949e7
Cary Huang <ckhuang@ucsc.edu>
2020-04-25 19:03:32 -0400

    sync

--- src/main.js
+++ src/main.js
@@ -12,8 +12,6 @@ let game = new Phaser.Game(config);
 let WIDTH = game.config.width;
 let HEIGHT = game.config.height;
 let gameOver = false;
-let spriteFrameWidth = 155.1547;
-let spriteFrameHeight = 92.6435;
 
 game.settings = {
     startSpeed: 0.2
--- src/scene/Play.js
+++ src/scene/Play.js
@@ -5,14 +5,14 @@ class Play extends Phaser.Scene {
 
     preload(){
         this.load.image('background', './assets/background.png');           // preload background
-        this.load.spritesheet('player', './assets/sprite4.png', {frameWidth: spriteFrameWidth, frameHeight: spriteFrameHeight, startFrame: 0, endFrame: 2});      //  preload character
+        this.load.spritesheet('player', './assets/sprite4.png', {frameWidth: 116.922, frameHeight: 139.3362, startFrame: 0, endFrame: 2});      //  preload character
     }
 
     create(){
         // place background
         this.background = this.add.tileSprite(0, 0, WIDTH, HEIGHT, 'background').setOrigin(0,0);
 
-        this.player = this.add.sprite(WIDTH/2 -46, HEIGHT - 120, 'player').setScale(0.5, 0.5).setOrigin(0,0);
+        this.player = this.add.sprite(WIDTH/2 -32, HEIGHT - 120, 'player').setScale(0.5, 0.5).setOrigin(0,0);
 
         this.anims.create({
             key: 'walk',
