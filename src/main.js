5f17e90381b53d3742acbf7064a6e4c5180312c0
Cary Huang <ckhuang@ucsc.edu>
2020-04-25 12:04:06 -0400

    Changed background dimensions and made it scroll

--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ let config = {
     type: Phaser.CANVAS,
     width: 640,
     height: 480,
+    scene: [ Play ]
 }
 
 // main game object
 let game = new Phaser.Game(config);
+
+let WIDTH = game.config.width;
+let HEIGHT = game.config.height;
+
+game.settings = {
+    startSpeed: 0.2
+}
--- src/scene/Play.js
+++ src/scene/Play.js
@@ -4,14 +4,14 @@ class Play extends Phaser.Scene {
     }
 
     preload(){
-
+        this.load.image('background', './assets/background.png');
     }
 
     create(){
-
+        this.background = this.add.tileSprite(0, 0, WIDTH, HEIGHT, 'background').setOrigin(0,0);
     }
 
     update(){
-        
+        this.background.tilePositionY -= game.settings.startSpeed;
     }
 }
