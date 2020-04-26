a5b12dbbd3e98e63a481232c1c4603cded64f955
Philip Cook <pecook@ucsc.edu>
2020-04-25 20:47:40 -0700

    menu update no difficulties set

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ let config = {
     type: Phaser.CANVAS,
     width: 600,
     height: 600,
-    scene: [ Play ]
+    scene: [Menu, Play  ]
 }
 
 // main game object
@@ -12,8 +12,9 @@ let game = new Phaser.Game(config);
 let WIDTH = game.config.width;
 let HEIGHT = game.config.height;
 let gameOver = false;
+game.easyMode = true;
 
-let keyLEFT, keyRIGHT;
+let keyLEFT, keyRIGHT, keyUP, keySPACE;
 
 game.settings = {
     startSpeed: 0.2
--- src/scene/Menu.js
+++ src/scene/Menu.js
@@ -3,14 +3,19 @@ class Menu extends Phaser.Scene {
         super("menuScene");
     }
     preload(){
-
+        this.load.image('menu', './assets/menu.png');
     }
 
     create(){
-
+        this.add.tileSprite(0, 0, 600, 600, 'menu').setOrigin(0, 0);
+        
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update(){
-        
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            this.scene.start("playScene");    
+        }
     }
 }
