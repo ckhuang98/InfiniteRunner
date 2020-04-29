4ccca5da462a3d0273bbb584a4ef0268308c62cd
Philip Cook <pecook@ucsc.edu>
2020-04-28 20:44:50 -0700

    Character Class Fix

--- src/prefabs/character.js
+++ src/prefabs/character.js
@@ -1,4 +1,5 @@
 // Character prefab
+// Character Fix
 class Character extends Phaser.Physics.Arcade.Sprite {
     constructor(scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
