44271cef0de9e853535d490c693fb5ee5a839928
Philip Cook <pecook@ucsc.edu>
2020-05-03 19:01:52 -0700

    Changed file names

--- src/scene/Menu.js
+++ src/scene/Menu.js
@@ -4,8 +4,8 @@ class Menu extends Phaser.Scene {
     }
     preload(){
         this.load.image('menu', './assets/menu.png');
-        this.load.audio('click', './assets/Click-SoundBible.com-1387633738.mp3');
-        this.load.audio('menuTheme', './assets/2018-02-24_-_Monster_In_The_Field_-_David_Fesliyan.mp3');
+        this.load.audio('click', './assets/press.mp3');
+        this.load.audio('menuTheme', './assets/menuSound.mp3');
     }
 
     create(){
